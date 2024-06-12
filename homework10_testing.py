a171d18d33e580a26aa3d50407756cf13702fa8e
Koenel <k20111151@kcl.ac.uk>
2024-06-12 16:34:15 +0200

    add testing file

---
+++ homework10_testing.py
@@ -0,0 +1,17 @@
+def evaluate(boardstring):
+    xxx = "xxx"
+    ooo = "ooo"
+    empty_space = " "
+    if xxx in boardstring: 
+        # this value is boardstring too because it is the input that will change everytime
+        return("X won")
+    elif ooo in boardstring:
+        # this value is boardstring too because it is the input that will change everytime
+        return("O won")
+    elif empty_space in boardstring: # note to self: default check mode is that an utterance is True.
+        return("game not over yet")
+    else:
+        return("draw")
+
+def move(board, mark, position):
+     return board[:position] + mark + board[position+1:] 

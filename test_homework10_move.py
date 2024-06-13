72b8a15f359ab1e84ac325f7b6b643573538df98
Koenel <k20111151@kcl.ac.uk>
2024-06-13 19:32:10 +0200

    add an actually working test for the move function

--- test_homework10_move.py
+++ test_homework10_move.py
@@ -1,4 +1,6 @@
 from homework10_testing import move, evaluate
 
 def test_move_1():
-    
+    boardie = "------xxo-----------"
+    result = move(boardie, "x", 1)
+    assert result =="-x----xxo-----------" 

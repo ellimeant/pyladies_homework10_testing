252bafcb7bed10b392c8f14e5884898da0219fbd
Koenel <k20111151@kcl.ac.uk>
2024-06-13 20:03:18 +0200

    new test to evaluate value error

--- test_homework10_evaluate.py
+++ test_homework10_evaluate.py
@@ -1,20 +1,27 @@
+import pytest
 from homework10_testing import move, evaluate
 
-def test_evaluate_1():
+def test_evaluate_x_wins():
     boardstring = "-------------xxx--"
     result = evaluate(boardstring)
     assert result == "X won"
     #assert len(20) == True
 
-def test_evaluate_2():
+def test_evaluate_continue_game():
     board = "HelloWord HelloWorld"
     res = evaluate(board)
     assert res == "game not over yet"
 
-def test_evaluate_3():
+def test_evaluate_draw():
     boardie = "ooxxooxxooxxoxooxxoo"
     res = evaluate(boardie)
     assert res == "draw"
+
+def test_valueerror():
+    a = "oooxoxxxooxxoxooxxoo"
+    with pytest.raises(ValueError):
+        evaluate(a)
+
 
 
 #this test does not test the function but just if the variable boardstring is contains "xxx".

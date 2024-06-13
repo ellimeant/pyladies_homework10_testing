cdfae152d853a3f598862d77608e862cd6c0347a
Koenel <k20111151@kcl.ac.uk>
2024-06-13 22:42:08 +0200

    second test to check if the function forbids using an index above 20. works!

--- test_homework10_move.py
+++ test_homework10_move.py
@@ -1,6 +1,15 @@
+import pytest
 from homework10_testing import move, evaluate
 
 def test_move_1():
+    #tests if the board is returned properly
     boardie = "------xxo-----------"
     result = move(boardie, "x", 1)
     assert result =="-x----xxo-----------" 
+
+def test_move_2():
+    #tests if function only accepts integers up to and including 20 as position - so that the string doesn't get any longer!
+    posit =21
+    boardstringie = "------xxo-----------"
+    with pytest.raises(ValueError):
+        move(boardstringie, "o", posit)

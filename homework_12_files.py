116d0d10fc84892c55aec8138a242cf982d37888
Koenel <k20111151@kcl.ac.uk>
2024-06-24 20:27:08 +0200

    testing the str to have string arguments. find out that there are too many words in the list, need to remove empty spaces

--- homework_12_files.py
+++ homework_12_files.py
@@ -3,20 +3,20 @@ poem_file = open('input.txt', encoding="utf-8")
 content = poem_file.read()
 print(content)
 
-characters = len(content) # characters in the poem string
+characters = str(len(content)) # characters in the poem string
 print("This poem contains", characters, "characters.")
 
-word_list = content.split()
+word_list = str(content.split())
 print("This poem has", len(word_list), "words.")
 
-many_words = len(word_list)
+many_words = str(len(word_list))
 
 
 specifics = content.count("and") # count specific words
 print("The word 'and' appears ",specifics, "times.")
 
 with open(r"input.txt", "r", encoding="utf-8") as goblin: # it needs the encoding too beacuse there seems to be an odd symbol somewhere.
-    lines = len(goblin.readlines())
+    lines = str(len(goblin.readlines()))
     print("Total number of lines: ", lines)
 
 
@@ -24,12 +24,19 @@ new_file = "output.txt"
 
 #I found out about the write() function: it only takes 1 argument at a time, and it must be a string. Hence, I modified the above.
 with open(new_file, "w", encoding="utf-8") as output_file:
-    output_file.write("The excerpt from Goblin Market contains")
-    output_file.write(many_words)
-    output_file.write("words. These words and all other elements of the text make up ")
+    output_file.write("The excerpt from Goblin Market contains ")
+    output_file.write(str(many_words))
+    output_file.write(" words. These words and all other elements of the text make up ")
     output_file.write(characters)
-    output_file.write("You can read them on")
+    output_file.write(" characters. You can read them on ")
     output_file.write(lines)
-    output_file.write("lines. Enjoy!")
+    output_file.write(" lines. Enjoy!")
 print(f"File{output_file} created successfully.")
+
+
+#test to open the output.txt file:
+output_successful = open('input.txt', encoding="utf-8")
+output_is_successful = poem_file.read()
+print(output_is_successful)
+
 #poem_file.close()          can't count lines in poem_file if this is in the code.

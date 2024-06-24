984f988b32053c12415665c58d7c350215b07c1d
Koenel <k20111151@kcl.ac.uk>
2024-06-24 21:13:06 +0200

    shortened down the read and write function in Task 3

--- homework_12_files.py
+++ homework_12_files.py
@@ -32,51 +32,33 @@ print(find_max(numbers))  # Expected output: Error message or appropriate handli
 
 #Task 3:
 
-#read in the file
-poem_file = open('input.txt', encoding="utf-8")
-content = poem_file.read()
-print(content)
-
-characters = str(len(content)) # characters in the poem string
-print("This poem contains", characters, "characters.")
-
-word_list = (content.split())
-print("This poem has", len(word_list), "words.")
-
-delete_char = " "
-while delete_char in word_list:
-    word_list.remove(delete_char)
-print(word_list)
-
-many_words = str(len(word_list))
-
-
-specifics = content.count("and") # count specific words
-print("The word 'and' appears ",specifics, "times.")
-
-with open(r"input.txt", "r", encoding="utf-8") as goblin: # it needs the encoding too beacuse there seems to be an odd symbol somewhere.
-    lines = str(len(goblin.readlines()))
+def input_to_output(text_file, outcome):
+    poem_file = open(text_file, encoding="utf-8")
+    content = poem_file.read()
+    print(content)
+    characters = str(len(content))
+    word_list = (content.split())
+    print("This poem contains", characters, "characters.")
+    delete_char = " "
+    while delete_char in word_list:
+        word_list.remove(delete_char)
+    print(word_list)
+    print("This poem has", len(word_list), "words.")
+    with open(text_file, "r", encoding="utf-8") as goblin:
+        lines = str(len(goblin.readlines()))
     print("Total number of lines: ", lines)
+    new_file = outcome
+    with open(new_file, "w", encoding="utf-8") as output_file:
+        output_file.write("The excerpt from Goblin Market contains ")
+        output_file.write(str(len(word_list)))
+        output_file.write(" words. These words and all other elements of the text make up ")
+        output_file.write(characters)
+        output_file.write(" characters. You can read them on ")
+        output_file.write(lines)
+        output_file.write(" lines. Enjoy!")
+    print(f"File{output_file} created successfully.")
 
-
-new_file = "output.txt"
-
-#I found out about the write() function: it only takes 1 argument at a time, and it must be a string. Hence, I modified the above.
-with open(new_file, "w", encoding="utf-8") as output_file:
-    output_file.write("The excerpt from Goblin Market contains ")
-    output_file.write(str(many_words))
-    output_file.write(" words. These words and all other elements of the text make up ")
-    output_file.write(characters)
-    output_file.write(" characters. You can read them on ")
-    output_file.write(lines)
-    output_file.write(" lines. Enjoy!")
-print(f"File{output_file} created successfully.")
-
-
-#test to open the output.txt file:
-output_successful = open('input.txt', encoding="utf-8")
-output_is_successful = poem_file.read()
-print(output_is_successful)
+input_to_output("input.txt", "output.txt")
 
 #poem_file.close()          can't count lines in poem_file if this is in the code.
 
